344dd0a5e153c4b5c1e69d3e65900a9ed6dff643
Pedro <phptrebil@gmail.com>
2023-02-07 21:29:18 -0300

    começando a trabalhar nos layouts

---
+++ app/src/main/java/br/com/pedro/agenda/ListaDeClientesActivity.kt
@@ -0,0 +1,11 @@
+package br.com.pedro.agenda
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaDeClientesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.lista_de_clientes_main)
+    }
+}
