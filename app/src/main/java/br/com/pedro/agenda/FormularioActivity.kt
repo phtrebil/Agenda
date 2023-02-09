4b026ee4385de7eefcd8c9b6cac7aa82f7756929
Pedro <phptrebil@gmail.com>
2023-02-08 21:21:12 -0300

    concluindo layouts de detalhes e formulário

---
+++ app/src/main/java/br/com/pedro/agenda/FormularioActivity.kt
@@ -0,0 +1,11 @@
+package br.com.pedro.agenda
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormularioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario)
+    }
+}
